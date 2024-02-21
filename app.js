3d86078cb697ecbe67d23e68ce113eb895eb8c15
Eslam Ibrahim <134802239+eslamibrahim23@users.noreply.github.com>
2024-02-21 21:54:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const User = require("./models/user.model");
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+//
 app.use(cors());
 app.use(express.json());
 app.get("/users", async (req, res) => {
